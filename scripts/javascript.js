5509b08ba9486fb59d59a80fe307aba5268040d9
tomeir <tsmeland@gmail.com>
2021-04-09 15:25:45 +0200

    added start button and cleared unused code

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,6 +1,10 @@
 var circleScale = document.getElementById('circlescale');
-var hyperVentFreq = document.getElementById('hyperventfreq')
+var hyperVentFreq = document.getElementById('hyperventfreq');
 var circle = document.getElementById('circle');
+var breathNumber = document.getElementById('hyperventnumber');
+var startButton = document.getElementById('startbtn');
+var circleNormalSize = 1;
+var i = 0;
 
 
 function changeHyperVentFreq () {
@@ -9,17 +13,9 @@ function changeHyperVentFreq () {
 
 function changeCircleScale() {
   circle.style.transform = "scale( " + circleScale.value + ")";
+  setTimeout(() => {circle.style.transform = "scale( " + circleNormalSize + ")";}, 2000);
 }
 
 
-function onKeyConfirm(event) {
-  if (circleScale.value.length > 0 && event.keyCode === 13) {
-    changeCircleScale();
-    circleScale.value = "";
-  }
-}
 
-
-addEventListener("input", changeHyperVentFreq);
-circleScale.addEventListener("input", changeCircleScale);
-//circleScale.addEventListener("keypress", onKeyConfirm);
+startButton.addEventListener('click', changeCircleScale);
