14c68917fe54ce67aaa8acb8b6b82cb08fd004b7
tomeir <tsmeland@gmail.com>
2021-04-20 19:55:15 +0200

    ...

---
+++ scripts/view.js
@@ -0,0 +1,22 @@
+
+var startBtn = document.getElementById('startbtn');
+var circle = document.getElementById('circle')
+
+
+
+function inputField() {
+  numberOfBreaths = document.getElementById('hyperventnumber').value;
+  console.log(numberOfBreaths);
+}
+
+function onClickStart() {
+  circle.style.transform = 'scale(2)';  
+}
+
+
+
+
+
+
+
+//startBtn.addEventListener("click", onClickStart());
