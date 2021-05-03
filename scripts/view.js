9a4e550d76bb6fdc4ffdd27acc0ff0355eeab5f3
tomeir <tsmeland@gmail.com>
2021-05-03 15:19:17 +0200

    la til ny funksjonalitet

--- scripts/view.js
+++ scripts/view.js
@@ -1,4 +1,7 @@
 var circle = document.getElementById('circle')
+var numberOfBreathsInput = document.getElementById('numberOfBreathsInput')
+var incrementBreathsBtn = document.getElementById('incrementBreaths')
+var subtractBreathsBtn = document.getElementById('subtractBreaths')
 
 /////////
 ///API///
@@ -18,3 +21,15 @@ function circleShrink(transitionTime) {
   circle.style.transform = 'scale(2)';
   circle.style.transition = 'all ' + transitionTime +'ms';      
 }
+
+
+function Increment () {
+  numberOfBreathsInput.value = Number(numberOfBreathsInput.value) + 1;
+}
+
+function Subtract () {
+  numberOfBreathsInput.value = Number(numberOfBreathsInput.value) - 1;
+}
+
+incrementBreathsBtn.addEventListener('click', Increment);
+subtractBreathsBtn.addEventListener('click', Subtract)
